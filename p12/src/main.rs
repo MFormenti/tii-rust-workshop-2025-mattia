d6f7726c1f4833d13d6b2f126432e08b4cae10ec
mattiaformenti <mattia.formenti@tii.ae>
2025-04-15 13:56:11 +0400

    commit p22

--- p12/src/main.rs
+++ p12/src/main.rs
@@ -1,3 +1,6 @@
+use p12::add;
 fn main() {
     println!("Hello, world!");
+    let x = add(1,2);
+    println!("1 + 2 = {}", x);
 }
---
+++ p22/src/main.rs
@@ -0,0 +1,5 @@
+mod song;
+fn main() {
+    use song::christmas_song;
+    println!("{}", christmas_song());
+}
